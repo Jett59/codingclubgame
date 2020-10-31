2959f0908e16ded0227573f591bd86dfe6e3a56c
jett59 <jett.thompson.59@gmail.com>
2020-10-31 19:16:06 +1100

    added variation in the location of the platforms

--- src/main/java/app/cleancode/game/platforms/PlatformCreator.java
+++ src/main/java/app/cleancode/game/platforms/PlatformCreator.java
@@ -1,5 +1,7 @@
 package app.cleancode.game.platforms;
 
+import java.util.Random;
+
 import app.cleancode.game.GameListener;
 import javafx.scene.shape.Rectangle;
 
@@ -10,15 +12,18 @@ public class PlatformCreator extends GameListener {
 	public static double platformWidth = 500;
 	public static double platformHeight = 12;
 	public static double platformY = 512;
+	public static double platformYVariation = 50;
 public PlatformsGameObject platforms;
 private int createdPlatforms = 0;
+private Random rand = new Random();
 
 	@Override
 	public void update() {
 		for(int i = 0; i < MAX_CONCURRENT_PLATFORM_GENERATIONS; i++) {
 			if(createdPlatforms <= numberOfPlatforms) {
 		double x = createdPlatforms*(platformWidth+platformSpacing);
-		Rectangle platform = new Rectangle(x, platformY, platformWidth, platformHeight);
+		double y = rand.nextDouble()*(2*platformY)+1;
+		Rectangle platform = new Rectangle(x, y, platformWidth, platformHeight);
 		platforms.addNode(platform);
 		platforms.platforms.add(platform);
 		createdPlatforms++;
